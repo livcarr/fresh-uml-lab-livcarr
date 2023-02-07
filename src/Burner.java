7fa0ee45eb38cd666db8d6b8929c56da02dcd7c9
livcarr <123025829+livcarr@users.noreply.github.com>
2023-02-06 21:09:44 -0500

    Update Burner.java

--- src/Burner.java
+++ src/Burner.java
@@ -7,17 +7,20 @@ public class Burner
   
   public Burner(double maxTemp, double minTemp, double diameter, double isHot)
   {
-  
+    maxTemp = this.maxTemp;
+    minTemp = this.minTemp;
+    diameter = this.diameter;
+    isHot = this.isHot;
   }
   
   public boolean turnOn()
   {
-  
+    return true;
   }
   
   public boolean turnOff()
   {
-  
+    return true;
   }
   
   public void increaseTemp()
@@ -32,21 +35,21 @@ public class Burner
   
   public double getDiameter()
   {
-  
+    return diameter;
   }
   
   public double getMaxTemp()
   {
-  
+    return maxTemp;
   }
   
   public double getMinTemp()
   {
-  
+    return minTemp;
   }
   
   public boolean isHot()
   {
-  
+    return hot;
   }
 }
