f229c74ee890fe28ed562506fd8443c0eaeab734
livcarr <123025829+livcarr@users.noreply.github.com>
2023-02-06 21:00:46 -0500

    Update Stove.java

--- src/Stove.java
+++ src/Stove.java
@@ -4,31 +4,36 @@ public class Stove
   
   public boolean hookup()
   {
-    
+    return true;
   }
   
   public setBurnerTemp(int burner, double temp)
   {
-  
+    burner = 0;
+    temp = 0;
+    return burner;
+    return temp;
   }
   
   public boolean isHot()
   {
-  
+    return true;
   }
   
   public boolean isHot(int burner)
   {
-  
+    return true;
   }
   
   public double getBurnerTemp(int burner)
   {
-  
+    temp = (double)burner;
+    return temp;
   }
   
   public Burner getBurner(int burner)
   {
-  
+    burner = (Burner)burner;
+    return burner;
   }
 }
