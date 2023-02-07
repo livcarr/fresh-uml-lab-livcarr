b295810ad969f48f3c4ccf48f7a50739a4c05e53
livcarr <123025829+livcarr@users.noreply.github.com>
2023-02-06 20:41:18 -0500

    Update electricBurner.java

--- src/electricBurner.java
+++ src/electricBurner.java
@@ -1,5 +1,27 @@
 
 public class electricBurner
 {
-
+  private double voltReq = 0;
+  private double ampReq = 0;
+  
+  
+  public electricBurner(double voltReq)
+  {
+  
+  }
+  
+  public void increaseCurrent()
+  {
+  
+  }
+  
+  public void decreaseCurrent()
+  {
+  
+  }
+  
+  public double getVoltReq()
+  {
+  
+  }
 }
